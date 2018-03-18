cfb09f1a5ddce1afac777c8c1a961534fb77c118
weizguy <dave.weizenegger@gmail.com>
2018-03-18 10:20:30 -0700

    added PostList component

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,50 @@
+<template>        
+    <section class="post-list">
+      <PostPreview 
+        id="1"
+        thumbnail="https://assets2.razerzone.com/images/rzr-ph/rzph-og-image-1200x630.jpg"
+        title="Razer Phone!"
+        previewText="The new razer phone is here!"
+      />
+      <PostPreview 
+        id="2"
+        thumbnail="https://cdn.guidingtech.com/media/assets/WordPress-Import/2017/01/razer2.png"
+        title="Project Valerie!"
+        previewText="Razer makes a triple screen laptop!"
+      />
+      <PostPreview 
+        id="3"
+        thumbnail="https://assets.razerzone.com/eeimages/razer_pages/15506/images/razer-christine-gallery-03.jpg"
+        title="Project Christine!"
+        previewText="Razer's modular concept computer!"
+      />
+      <PostPreview 
+        id="4"
+        thumbnail="https://assets.razerzone.com/eeimages/projectlinda/projectlinda_2018_og_image.jpg"
+        title="Project Linda!"
+        previewText="Razer combines phone with laptop!"
+      />
+    </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  }
+}
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -3,41 +3,16 @@
     <section class="intro">
       <h1>Get the latest Razer news!</h1>
     </section>
-    <section class="featured-posts">
-      <PostPreview 
-        id="1"
-        thumbnail="https://assets2.razerzone.com/images/rzr-ph/rzph-og-image-1200x630.jpg"
-        title="Razer Phone!"
-        previewText="The new razer phone is here!"
-      />
-      <PostPreview 
-        id="2"
-        thumbnail="https://cdn.guidingtech.com/media/assets/WordPress-Import/2017/01/razer2.png"
-        title="Project Valerie!"
-        previewText="Razer makes a triple screen laptop!"
-      />
-      <PostPreview 
-        id="3"
-        thumbnail="https://assets.razerzone.com/eeimages/razer_pages/15506/images/razer-christine-gallery-03.jpg"
-        title="Project Christine!"
-        previewText="Razer's modular concept computer!"
-      />
-      <PostPreview 
-        id="4"
-        thumbnail="https://assets.razerzone.com/eeimages/projectlinda/projectlinda_2018_og_image.jpg"
-        title="Project Linda!"
-        previewText="Razer combines phone with laptop!"
-      />
-    </section>
+    <PostList />
   </div>
 </template>
 
 <script>
-import PostPreview from '@/components/Posts/PostPreview'
+import PostList from '@/components/Posts/PostList'
 
 export default {
   components: {
-    PostPreview
+    PostList
   }
 }
 </script>
